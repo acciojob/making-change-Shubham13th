9b65c5893dd6af32afd3369e358fd703b771d453
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-22 14:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const makeChange = (c) => {
   // your name here
+	let p = 1;
+    let n = 5;
+    let d = 10;
+    let q = 25;
+
+    let result = { "q": 0, "d": 0, "n": 0, "p": 0 };
+
+    while (c > 0) {
+    if (c >= 25) {
+      c -= 25;
+      result.q++;
+    } else if (c >= 10) {
+      c -= 10;
+      result.d++;
+    } else if (c >= 5) {
+      c -= 5;
+      result.n++;
+    } else {
+      c -= 1;
+      result.p++;
+    }
+  }
+
+  return result;
 };
 
 // Do not the change the code below
